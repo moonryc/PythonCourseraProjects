60779f946cf0a126dfde113eeae2f367eef3776f
moonryc <rycmoon@gmail.com>
2021-03-21 17:11:22 -0500

    initial commit

---
+++ AT/Project1/question_5.py
@@ -0,0 +1,35 @@
+import random
+import main
+import matplotlib.pyplot as plt
+#import user48_LAdJVaadjQ_7 as main
+#import codeskulptor
+#import simpleplot
+#codeskulptor.set_timeout(30)
+
+
+#In this last problem, we will compare the in-degree distribution for the citation graph to the in-degree distribution for the DPA graph 
+#as constructed in Question 4. In particular, we will consider whether the shape of these two distributions are similar and, if they are 
+#similar, what might be the cause of the similarity.
+#
+#To help you in your analysis, you should consider the following three phenomena:
+#
+#    -The "six degrees of separation" phenomenon,
+once you exceed one connection per node the chance of connection exceeds dramatically, every node allmost links up so if you select a nod 
+almost all of the other nodes with come with it
+buttons are a great way to explain this
+#
+#    -The "rich gets richer" phenomenon, and
+#
+#    -The "Hierarchical structure of networks" phenomenon.
+#
+#If you're not familiar with these phenomena, you can read about them by conducting a simple Google or Wikipedia search. Your task for this problem is to consider how one of these phenomena might explain the structure of the citation graph or, alternatively, how the citations patterns follow one of these phenomena. 
+#
+#When answering this question, please include answers to the following:
+#
+#    -Is the plot of the in-degree distribution for the DPA graph similar to that of the citation graph? Provide a short explanation of the 
+#        similarities or differences. Focus on the various properties of the two plots as discussed in the class page on 
+#        "Creating, formatting, and comparing plots".
+#
+#    -Which one of the three social phenomena listed above mimics the behavior of the DPA process? Provide a short explanation for your answer.
+#
+#    -Could one of these phenomena explain the structure of the physics citation graph? Provide a short explanation for your answer.
