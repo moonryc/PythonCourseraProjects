4f6ccd5cf391a13c4d19ac24d0c500a44b29b928
moonryc <rycmoon@gmail.com>
2021-03-31 21:06:24 -0500

    pylint update

--- AT2/project3/algo_project_3_testsuit.py
+++ AT2/project3/algo_project_3_testsuit.py
@@ -43,6 +43,7 @@ def load_data_table(data_url):
 #########################################################################
 # Helper function for converting list of clusters to set of county tuples
 
+
 def set_of_county_tuples(cluster_list):
     """
     Input: A list of Cluster objects
@@ -51,7 +52,7 @@ def set_of_county_tuples(cluster_list):
     set_of_clusters = set([])
     for cluster in cluster_list:
         counties_in_cluster = cluster.fips_codes()
-        
+
         # convert to immutable representation before adding to set
         county_tuple = tuple(sorted(list(counties_in_cluster)))
         set_of_clusters.add(county_tuple)
@@ -66,13 +67,12 @@ def test_hierarchical24():
     Test for hierarchical clustering
     Note that hierarchical_clustering mutates cluster_list
     """
-    
+
     # load small data table
     print()
-    print( "Testing hierarchical_clustering on 24 county set")
+    print("Testing hierarchical_clustering on 24 county set")
     data_24_table = load_data_table(DATA_24_URL)
-    
-    
+
     # test data of the form [size of output cluster, sets of county tuples]
     hierdata_24 = [[23, set([('11001', '51013'), ('01073',), ('06059',), ('06037',), ('06029',), ('06071',), ('06075',), ('08031',), ('24510',), ('34013',), ('34039',), ('34017',), ('36061',), ('36005',), ('36047',), ('36059',), ('36081',), ('41051',), ('41067',), ('51840',), ('51760',), ('55079',), ('54009',)])],
                    [22, set([('11001', '51013'), ('36047', '36081'), ('01073',), ('06059',), ('06037',), ('06029',), ('06071',), ('06075',), ('08031',), ('24510',), ('34013',), ('34039',), ('34017',), ('36061',), ('36005',), ('36059',), ('41051',), ('41067',), ('51840',), ('51760',), ('55079',), ('54009',)])],
@@ -98,31 +98,36 @@ def test_hierarchical24():
                    [2, set([('01073', '11001', '24510', '34013', '34017', '34039', '36005', '36047', '36059', '36061', '36081', '51013', '51760', '51840', '54009', '55079'), ('06029', '06037', '06059', '06071', '06075', '08031', '41051', '41067')])],
                    ]
 
-        
     suite = poc_simpletest.TestSuite()
-    
+
     for num_clusters, expected_county_tuple in hierdata_24:
-        
+
         # build initial list of clusters for each test since mutation is allowed
         cluster_list = []
         for idx in range(len(data_24_table)):
             line = data_24_table[idx]
-            cluster_list.append(alg_cluster.Cluster(set([line[0]]), line[1], line[2], line[3], line[4]))
+            cluster_list.append(alg_cluster.Cluster(
+                set([line[0]]),
+                line[1],
+                line[2],
+                line[3],
+                line[4]))
 
         # compute student answer
         student_clustering = student.hierarchical_clustering(cluster_list, num_clusters)
         student_county_tuple = set_of_county_tuples(student_clustering)
-        
+
         # Prepare test
-        error_message = "Testing hierarchical_clustering on 24 county table, num_clusters = " + str(num_clusters)
+        error_message = "Testing hierarchical_clustering on 24 county table, num_clusters = " + str(
+            num_clusters)
         error_message += "\nStudent county tuples: " + str(student_county_tuple)
         error_message += "\nExpected county tuples: " + str(expected_county_tuple)
         suite.run_test(student_county_tuple == expected_county_tuple, True, error_message)
 
     suite.report_results()
 
-test_hierarchical24()
 
+test_hierarchical24()
 
 
 def test_kmeans():
@@ -130,13 +135,13 @@ def test_kmeans():
     Test for k-means clustering
     kmeans_clustering should not mutate cluster_list, but make a new copy of each test anyways
     """
-    
+
     # load small data table
     print()
-    print( "Testing kmeans_clustering on 24 county set")
+    print("Testing kmeans_clustering on 24 county set")
     data_24_table = load_data_table(DATA_24_URL)
-        
-    kmeansdata_24 = [[15, 1, set([('34017', '36061'), ('06037',), ('06059',), ('36047',), ('36081',), ('06071', '08031'), ('36059',), ('36005',), ('55079',), ('34013', '34039'), ('06075',), ('01073',), ('06029',), ('41051', '41067'), ('11001', '24510', '51013', '51760', '51840', '54009')])], 
+
+    kmeansdata_24 = [[15, 1, set([('34017', '36061'), ('06037',), ('06059',), ('36047',), ('36081',), ('06071', '08031'), ('36059',), ('36005',), ('55079',), ('34013', '34039'), ('06075',), ('01073',), ('06029',), ('41051', '41067'), ('11001', '24510', '51013', '51760', '51840', '54009')])],
                      [15, 3, set([('34017', '36061'), ('06037', '06059'), ('06071',), ('36047',), ('36081',), ('08031',), ('36059',), ('36005',), ('55079',), ('34013', '34039'), ('06075',), ('01073',), ('06029',), ('41051', '41067'), ('11001', '24510', '51013', '51760', '51840', '54009')])],
                      [15, 5, set([('34017', '36061'), ('06037', '06059'), ('06071',), ('36047',), ('36081',), ('08031',), ('36059',), ('36005',), ('55079',), ('34013', '34039'), ('06075',), ('01073',), ('06029',), ('41051', '41067'), ('11001', '24510', '51013', '51760', '51840', '54009')])],
                      [10, 1, set([('34017', '36061'), ('06029', '06037', '06075'), ('11001', '24510', '34013', '34039', '51013', '51760', '51840', '54009'), ('06059',), ('36047',), ('36081',), ('06071', '08031', '41051', '41067'), ('36059',), ('36005',), ('01073', '55079')])],
@@ -144,31 +149,35 @@ def test_kmeans():
                      [10, 5, set([('34013', '34017', '36061'), ('06029', '06037', '06075'), ('08031', '41051', '41067'), ('06059', '06071'), ('34039', '36047'), ('36081',), ('36059',), ('36005',), ('01073', '55079'), ('11001', '24510', '51013', '51760', '51840', '54009')])],
                      [5, 1, set([('06029', '06037', '06075'), ('01073', '11001', '24510', '34013', '34017', '34039', '36047', '51013', '51760', '51840', '54009', '55079'), ('06059',), ('36005', '36059', '36061', '36081'), ('06071', '08031', '41051', '41067')])],
                      [5, 3, set([('06029', '06037', '06075'), ('11001', '24510', '34013', '34017', '34039', '36005', '36047', '36059', '36061', '36081', '51013'), ('08031', '41051', '41067'), ('06059', '06071'), ('01073', '51760', '51840', '54009', '55079')])],
-                     [5, 5, set([('06029', '06037', '06075'), ('08031', '41051', '41067'), ('06059', '06071'), ('01073', '55079'), ('11001', '24510', '34013', '34017', '34039', '36005', '36047', '36059', '36061', '36081', '51013', '51760', '51840', '54009')])]]    
-        
-    suite = poc_simpletest.TestSuite()    
-    
+                     [5, 5, set([('06029', '06037', '06075'), ('08031', '41051', '41067'), ('06059', '06071'), ('01073', '55079'), ('11001', '24510', '34013', '34017', '34039', '36005', '36047', '36059', '36061', '36081', '51013', '51760', '51840', '54009')])]]
+
+    suite = poc_simpletest.TestSuite()
+
     for num_clusters, num_iterations, expected_county_tuple in kmeansdata_24:
-        
+
         # build initial list of clusters for each test since mutation is allowed
         cluster_list = []
         for idx in range(len(data_24_table)):
             line = data_24_table[idx]
-            cluster_list.append(alg_cluster.Cluster(set([line[0]]), line[1], line[2], line[3], line[4]))
+            cluster_list.append(alg_cluster.Cluster(
+                set([line[0]]),
+                line[1],
+                line[2],
+                line[3],
+                line[4]))
 
         # compute student answer
         student_clustering = student.kmeans_clustering(cluster_list, num_clusters, num_iterations)
         student_county_tuple = set_of_county_tuples(student_clustering)
-        
+
         # Prepare test
-        error_message = "Testing kmeans_custering on 24 county table, num_clusters = " + str(num_clusters)
+        error_message = "Testing kmeans_custering on 24 county table, num_clusters = " + str(
+            num_clusters)
         error_message += " num_iterations = " + str(num_iterations)
         error_message += "\nStudent county tuples: " + str(student_county_tuple)
         error_message += "\nExpected county tuples: " + str(expected_county_tuple)
-        suite.run_test(student_county_tuple == expected_county_tuple, True, error_message)   
+        suite.run_test(student_county_tuple == expected_county_tuple, True, error_message)
 
     suite.report_results()
-    
-#test_kmeans()
-        
-        
+
+# test_kmeans()
