8d1c895b5dd8b5b80bbc23cce1d6546efda64a09
moonryc <rycmoon@gmail.com>
2021-04-05 22:02:19 -0500

    kmeans_clustering is now finished and hierchical clustering has been improved

--- AT2/project3/main.py
+++ AT2/project3/main.py
@@ -1,5 +1,4 @@
 """
-
 slow_closest_pair(cluster_list)
 fast_closest_pair(cluster_list)
 closest_pair_strip(cluster_list, horiz_center, half_width)
@@ -8,7 +7,7 @@ kmeans_clustering(cluster_list, num_clusters, num_iterations)
 
 where cluster_list is a 2D list of clusters in the plane
 """
-# pylint: disable=C0301
+
 import math
 import alg_cluster
 
@@ -49,6 +48,7 @@ def slow_closest_pair(cluster_list):
                     coordinates = [i_temp, j_temp]
                     coordinates.sort()
                     distance = d_temp
+
     return (distance, coordinates[0], coordinates[1])
 
 
@@ -107,11 +107,11 @@ def closest_pair_strip(cluster_list, horiz_center, half_width):
         if abs(cluster_list[i_value].horiz_center() - horiz_center) < half_width:
             s_cluster_list.append([cluster_list[i_value], i_value])
     s_cluster_list.sort(key=lambda cluster: cluster[0].vert_center())
-    k = len(s_cluster_list)
+    length_k = len(s_cluster_list)
     distance, i_value, j_value = float('inf'), -1, -1
     coordinates = [i_value, j_value]
-    for idx1 in range(k-1):
-        number = min([idx1+4, k])
+    for idx1 in range(length_k-1):
+        number = min([idx1+4, length_k])
         for idx2 in range(idx1+1, number):
             temp_distance, temp_i_value, temp_j_value = pair_distance(
                 cluster_list, s_cluster_list[idx1][1], s_cluster_list[idx2][1])
@@ -119,6 +119,7 @@ def closest_pair_strip(cluster_list, horiz_center, half_width):
                 coordinates = [temp_i_value, temp_j_value]
                 coordinates.sort()
                 distance = temp_distance
+
     return (distance, coordinates[0], coordinates[1])
 
 
@@ -141,19 +142,19 @@ def hierarchical_clustering(cluster_list, num_clusters):
     Output: List of clusters whose length is num_clusters
     """
 
-    #cluster_list.sort(key = lambda cluster: cluster.vert_center())
-
+    cluster_list.sort(key=lambda cluster: cluster.horiz_center())
     while len(cluster_list) > num_clusters:
         (dummy, cluster_i, cluster_j) = fast_closest_pair(cluster_list)
         cluster_list[cluster_i].merge_clusters(cluster_list[cluster_j])
         cluster_list.remove(cluster_list[cluster_j])
+        cluster_list.sort(key=lambda cluster: cluster.horiz_center())
     return cluster_list
 
 
-print(hierarchical_clustering(
-    [alg_cluster.Cluster(set(['']), 0, 0, 1, 1),
-     alg_cluster.Cluster(set(['']), 2, 3, 1, 1),
-     alg_cluster.Cluster(set(['']), 2, 0, 1, 1)], 2))
+# #print(hierarchical_clustering(
+#     [alg_cluster.Cluster(set(['']), 0, 0, 1, 1),
+#      alg_cluster.Cluster(set(['']), 2, 3, 1, 1),
+#      alg_cluster.Cluster(set(['']), 2, 0, 1, 1)], 2))
 ######################################################################
 # Code for k-means clustering
 
@@ -169,27 +170,49 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
 
     # position initial clusters at the location of clusters with largest populations
 
-    return []
+    population_and_index = [(cluster_list[idx].total_population(), idx)
+                            for idx in range(len(cluster_list))]
+    population_and_index.sort()
+    population_and_index.reverse()
+    mean_cluster = [(cluster_list[population_and_index[idx][1]].horiz_center(),
+                     cluster_list[population_and_index[idx][1]].vert_center())
+                    for idx in range(num_clusters)]
+
+    new_cluster_list = []
+    for dummy_i in range(num_iterations):
+        # initialize k empty sets C1,.....,Ck
+        mean_cluster_list = []
+        new_cluster_list = []
+        for i_index in range(num_clusters):
+            mean_cluster_list.append(
+                alg_cluster.Cluster(
+                    set([]),
+                    mean_cluster[i_index][0],
+                    mean_cluster[i_index][1],
+                    0, 0))
+            new_cluster_list.append(
+                alg_cluster.Cluster(
+                    set([]),
+                    mean_cluster[i_index][0],
+                    mean_cluster[i_index][1],
+                    0, 0))
+        # This was for creating the new_cluster_list
+
+        for idx1 in range(len(cluster_list)):
+            temp_dist = []
+            for idx2 in range(num_clusters):
+                temp_dist.append((cluster_list[idx1].distance(
+                    mean_cluster_list[idx2]), idx2))
+            new_cluster_list[min(temp_dist)[1]].merge_clusters(cluster_list[idx1])
+
+        # This is for redefining the new_clusters center
+
+        mean_cluster = [(new_cluster_list[idx].horiz_center(), new_cluster_list[idx].vert_center())
+                        for idx in range(num_clusters)]
+
+    return new_cluster_list
 
 
-TEST_CASE = [
-    alg_cluster.Cluster(set(['00']), 0.0, 0.0, 1, 0.1),
-
-
-    alg_cluster.Cluster(set(['10']), 1.0, 0.0, 2, 0.1),
-
-
-    alg_cluster.Cluster(set(['11']), 1.0, 1.0, 3, 0.1),
-
-
-    alg_cluster.Cluster(set(['01']), 0.0, 1.0, 4, 0.1),
-
-
-    alg_cluster.Cluster(set(['1010']), 10.0, 10.0, 5, 0.1),
-
-
-    alg_cluster.Cluster(set(['1011']), 10.0, 11.0, 6, 0.1),
-
-    alg_cluster.Cluster(set(['1111']), 11.0, 11.0, 7, 0.1),
-
-    alg_cluster.Cluster(set(['1110']), 11.0, 10.0, 8, 0.1)]
+print(kmeans_clustering([alg_cluster.Cluster(set(['5']), 10, 5, 1, 1),
+                         alg_cluster.Cluster(set(['3']), 2, 3, 120, 1),
+                         alg_cluster.Cluster(set(['1']), 2, 0, 100, 1)], 2, 3))
