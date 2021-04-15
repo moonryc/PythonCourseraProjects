9c4e5b1f8575033e4f460bc0d032cad8c2d0c27d
moonryc <rycmoon@gmail.com>
2021-04-15 12:55:42 -0500

    fixed an error in question 7

--- AT2/project4/question_7.py
+++ AT2/project4/question_7.py
@@ -14,7 +14,7 @@ scoring_matrix = main.build_scoring_matrix(alphabet,2,1,0)
 alignment_matrix = main.compute_alignment_matrix(seq_x, seq_y,scoring_matrix,True)
 alignment = main.compute_global_alignment(seq_x, seq_y,scoring_matrix,alignment_matrix)
 print(alignment)
-x_l = len(seq_x)-1
-y_l = len(seq_y)-1
+x_l = len(seq_x)
+y_l = len(seq_y)
 print('the edit distance is: ' ,x_l +y_l - alignment[0])
 print('The answer is: (diagonal_score:2, off_diag_score:1, dash_score:0)')
